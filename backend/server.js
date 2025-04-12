8644757b068bdd67b25bacdd77e21103cb6b8706
David Uziel <daviduziel.gh@gmail.com>
2025-04-11 19:46:14 -0600

    Added model to backend

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import * as tf from '@tensorflow/tfjs-node'
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.json());
+
+let model;
+async function loadModel() {
+	const modelPath = path.join(__dirname, 'model', 'model.json');
+	model = await tf.loadLayersModel(`file://${modelPath}`);
+}
+loadModel();
+
+app.post('/predict', async (req, res) => {
+	try{
+		const data = req.body.data;
+		const inputTensor = tf.tensor([data]);
+		const prediction = model.predict(inputTensor);
+		const result = Array.from(prediction.dataSync());
+		res.json({ prediction: result});
+	}catch(err){
+		res.status(500).json({error: err.message})
+	}
+});
+
+const PORT = 3001;
+app.listen(PORT, async () => {
+	await loadModel();
+	console.log(`Servidor en http://localhost${PORT}`);
+})
