9fd8384df650e85e982f9b8ec7a4b2cf42265320
ButterBug <86893740+ButterBug404@users.noreply.github.com>
2025-05-18 10:15:56 -0600

    App.js

--- frontend/App.js
+++ frontend/App.js
@@ -8,6 +8,9 @@ import {
   StyleSheet,
   Image,
   ScrollView,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -26,38 +29,9 @@ import styles from './Components/estilos.js';
 // Lista de estados
 const estados = [
   'Selecciona Estado',
-  'Aguascalientes',
-  'Baja California',
-  'Baja California Sur',
-  'Campeche',
-  'Chiapas',
-  'Chihuahua',
   'Ciudad de México',
-  'Coahuila',
-  'Colima',
-  'Durango',
   'Estado de México',
-  'Guanajuato',
-  'Guerrero',
-  'Hidalgo',
   'Jalisco',
-  'Michoacán',
-  'Morelos',
-  'Nayarit',
-  'Nuevo León',
-  'Oaxaca',
-  'Puebla',
-  'Querétaro',
-  'Quintana Roo',
-  'San Luis Potosí',
-  'Sinaloa',
-  'Sonora',
-  'Tabasco',
-  'Tamaulipas',
-  'Tlaxcala',
-  'Veracruz',
-  'Yucatán',
-  'Zacatecas',
 ];
 
 const Tab = createBottomTabNavigator();
@@ -66,18 +40,73 @@ const HomeScreen = () => <Inicio />;
 const MascotasScreen = () => <Mascotas />;
 const RegistrarScreen = () => <Registrar />;
 const AdoptaScreen = () => <Adoptar />;
-const PerfilScreen = () => <Cuenta />;
+const PerfilScreen = ({ setIsLoggedIn }) => (
+  <Cuenta setIsLoggedIn={setIsLoggedIn} />
+);
 
 const App = () => {
   const [selectedEstado, setSelectedEstado] = useState('');
   const [isSignUp, setIsSignUp] = useState(false); // Alternar entre inicio y registro
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado para manejar el login
+  const [correo, setCorreo] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [telefono, setTelefono] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [passwordError, setPasswordError] = useState('');
   const [showPassword, setShowPassword] = useState(false); // Estado para mostrar/ocultar contraseña
 
-  const PerfilScreen = () => <Cuenta setIsLoggedIn={setIsLoggedIn} />;
+  const validatePassword = (password) => {
+    const passwordPattern = /^(?=.*[A-Z]).{6,}$/;
+    return passwordPattern.test(password);
+  };
+
+  const handleRegister = () => {
+    if (
+      !correo ||
+      !password ||
+      !nombre ||
+      !telefono ||
+      selectedEstado === 'Selecciona Estado'
+    ) {
+      Alert.alert(
+        'Error',
+        'Por favor, completa todos los campos correctamente.'
+      );
+    } else if (!validatePassword(password)) {
+      setPasswordError(
+        'La contraseña debe tener al menos 6 caracteres y una mayúscula.'
+      );
+    } else if (password !== confirmPassword) {
+      setPasswordError('La contraseña no coincide.');
+    } else {
+      Alert.alert('Registro exitoso', 'Se ha registrado exitosamente.');
+      setIsSignUp(false); // Volver a la pantalla de inicio de sesión
+      setCorreo('');
+      setPassword('');
+      setConfirmPassword('');
+      setNombre('');
+      setTelefono('');
+      setSelectedEstado('Selecciona Estado');
+      setPasswordError('');
+    }
+  };
+
+  const handleLogin = () => {
+    if (!correo || !password) {
+      Alert.alert(
+        'Error',
+        'Por favor, completa todos los campos correctamente.'
+      );
+    } else if (!validatePassword(password)) {
+      setPasswordError(
+        'La contraseña debe tener al menos 6 caracteres y una mayúscula.'
+      );
+    } else {
+      setIsLoggedIn(true);
+      setPasswordError('');
+    }
+  };
 
   if (!isLoggedIn) {
     return (
@@ -86,61 +115,108 @@ const App = () => {
           <ImageBackground
             source={require('./assets/dddepth-169.jpg')}
             style={styles.card}
-            resizeMode="cover">
-            <ScrollView>
-              <Image
-                source={require('./assets/Mod1.gif')}
-                style={styles.logoApp}
-              />
-
-              <Text style={styles.title}>Laika</Text>
-
-              <Text style={styles.subtitle2}>
-                buscaremos a tu peludito en todo el universo.
-              </Text>
-              <Text style={styles.subtitle}>
-                {isSignUp
-                  ? 'Regístrate para buscar a tu peludito.'
-                  : 'Inicia sesión para continuar.'}
-              </Text>
-              <TextInput
-                placeholder="Correo electrónico"
-                style={styles.input}
-                placeholderTextColor="#888888"
-              />
-              <View style={styles.passwordContainer}>
-                <TextInput
-                  placeholder="Contraseña"
-                  secureTextEntry={!showPassword} // Mostrar u ocultar contraseña
-                  style={styles.input}
-                  placeholderTextColor="#888888"
-                  value={password}
-                  onChangeText={(text) => {
-                    setPassword(text);
-                    if (confirmPassword && text !== confirmPassword) {
-                      setPasswordError('La contraseña no coincide');
-                    } else {
-                      setPasswordError('');
-                    }
-                  }}
+            resizeMode="cover"
+            imageStyle={styles.backgroundImage}>
+            <KeyboardAvoidingView
+              behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+              style={styles.keyboardAvoidingView}>
+              <ScrollView contentContainerStyle={styles.scrollContentContainer}>
+                <Image
+                  source={require('./assets/Mod1.gif')}
+                  style={styles.logoApp}
                 />
-                <TouchableOpacity
-                  onPress={() => setShowPassword(!showPassword)}
-                  style={styles.iconContainer}>
-                  <Ionicons
-                    name={showPassword ? 'eye-off' : 'eye'}
-                    size={24}
-                    color="#888888"
+
+                <Text style={styles.title}>Laika</Text>
+
+                <Text style={styles.subtitle2}>
+                  buscaremos a tu peludito en todo el universo.
+                </Text>
+                <Text style={styles.subtitle}>
+                  {isSignUp
+                    ? 'Regístrate para buscar a tu peludito.'
+                    : 'Inicia sesión para continuar.'}
+                </Text>
+                {isSignUp && (
+                  <>
+                    <TextInput
+                      placeholder="Nombre completo"
+                      style={styles.input}
+                      placeholderTextColor="#888888"
+                      value={nombre}
+                      onChangeText={(text) => setNombre(text)}
+                    />
+                    <TextInput
+                      placeholder="Correo electrónico"
+                      style={styles.input}
+                      placeholderTextColor="#888888"
+                      value={correo}
+                      onChangeText={(text) => setCorreo(text)}
+                    />
+                    <TextInput
+                      placeholder="Teléfono"
+                      keyboardType="phone-pad"
+                      style={styles.input}
+                      placeholderTextColor="#888888"
+                      value={telefono}
+                      onChangeText={(text) => setTelefono(text)}
+                    />
+                    <Picker
+                      selectedValue={selectedEstado}
+                      style={styles.picker}
+                      onValueChange={(itemValue) =>
+                        setSelectedEstado(itemValue)
+                      }>
+                      {estados.map((estado, index) => (
+                        <Picker.Item
+                          key={index}
+                          label={estado}
+                          value={estado}
+                        />
+                      ))}
+                    </Picker>
+                  </>
+                )}
+                {!isSignUp && (
+                  <TextInput
+                    placeholder="Correo electrónico"
+                    style={styles.input}
+                    placeholderTextColor="#888888"
+                    value={correo}
+                    onChangeText={(text) => setCorreo(text)}
                   />
-                </TouchableOpacity>
-              </View>
-              {isSignUp && (
-                <>
+                )}
+                <View style={styles.passwordContainer}>
+                  <TextInput
+                    placeholder="Contraseña"
+                    secureTextEntry={!showPassword} // Mostrar u ocultar contraseña
+                    style={styles.inputPassword}
+                    placeholderTextColor="#888888"
+                    value={password}
+                    onChangeText={(text) => {
+                      setPassword(text);
+                      if (confirmPassword && text !== confirmPassword) {
+                        setPasswordError('La contraseña no coincide');
+                      } else {
+                        setPasswordError('');
+                      }
+                    }}
+                  />
+                  <TouchableOpacity
+                    onPress={() => setShowPassword(!showPassword)}
+                    style={styles.iconContainer}>
+                    <Ionicons
+                      name={showPassword ? 'eye-off' : 'eye'}
+                      size={24}
+                      color="#000000"
+                    />
+                  </TouchableOpacity>
+                </View>
+                {isSignUp && (
                   <View style={styles.passwordContainer}>
                     <TextInput
                       placeholder="Confirmar contraseña"
                       secureTextEntry={!showPassword} // Mostrar u ocultar contraseña
-                      style={styles.input}
+                      style={styles.inputPassword}
                       placeholderTextColor="#888888"
                       value={confirmPassword}
                       onChangeText={(text) => {
@@ -158,43 +234,43 @@ const App = () => {
                       <Ionicons
                         name={showPassword ? 'eye-off' : 'eye'}
                         size={24}
-                        color="#888888"
+                        color="#000000"
                       />
                     </TouchableOpacity>
                   </View>
-                  {passwordError ? (
-                    <Text style={styles.errorText}>{passwordError}</Text>
-                  ) : null}
-                </>
-              )}
+                )}
+                {passwordError ? (
+                  <Text style={styles.errorText}>{passwordError}</Text>
+                ) : null}
+                {isSignUp && (
+                  <Text style={styles.passwordHint}>
+                    La contraseña debe tener al menos 6 caracteres y una
+                    mayúscula.
+                  </Text>
+                )}
 
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => {
-                  if (isSignUp && password !== confirmPassword) {
-                    setPasswordError('La contraseña no coincide');
-                  } else {
-                    setIsLoggedIn(true);
-                  }
-                }}
-                disabled={
-                  isSignUp &&
-                  (password !== confirmPassword ||
-                    !password ||
-                    !confirmPassword)
-                }>
-                <Text style={styles.buttonText}>
-                  {isSignUp ? 'Registrarse' : 'Iniciar Sesión'}
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => setIsSignUp(!isSignUp)}>
-                <Text style={styles.toggleText}>
-                  {isSignUp
-                    ? '¿Ya tienes cuenta? Inicia sesión'
-                    : '¿No tienes cuenta? Regístrate'}
-                </Text>
-              </TouchableOpacity>
-            </ScrollView>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={isSignUp ? handleRegister : handleLogin}
+                  disabled={
+                    isSignUp &&
+                    (password !== confirmPassword ||
+                      !password ||
+                      !confirmPassword)
+                  }>
+                  <Text style={styles.buttonText}>
+                    {isSignUp ? 'Registrarse' : 'Iniciar Sesión'}
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => setIsSignUp(!isSignUp)}>
+                  <Text style={styles.toggleText}>
+                    {isSignUp
+                      ? '¿Ya tienes cuenta? Inicia sesión'
+                      : '¿No tienes cuenta? Regístrate'}
+                  </Text>
+                </TouchableOpacity>
+              </ScrollView>
+            </KeyboardAvoidingView>
           </ImageBackground>
         </SafeAreaView>
       </SafeAreaProvider>
@@ -236,14 +312,16 @@ const App = () => {
           tabBarInactiveTintColor: '#777',
           headerTitle: 'Laika', // Cambia el título del header a "Laika"
           headerTitleAlign: 'center', // Centra el título
-          headerStyle: { backgroundColor: 'indianred' },
-          headerTintColor: 'white',
+          headerStyle: { backgroundColor: '#b04f4f' },
+          headerTintColor: '#f6d3c3',
         })}>
-        <Tab.Screen name="Perfil" component={PerfilScreen} />
         <Tab.Screen name="Inicio" component={HomeScreen} />
         <Tab.Screen name="Mascotas" component={MascotasScreen} />
         <Tab.Screen name="Registrar" component={RegistrarScreen} />
         <Tab.Screen name="Adopción" component={AdoptaScreen} />
+        <Tab.Screen name="Perfil">
+          {() => <PerfilScreen setIsLoggedIn={setIsLoggedIn} />}
+        </Tab.Screen>
       </Tab.Navigator>
     </NavigationContainer>
   );
